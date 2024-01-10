6be74ae8df006f0fa97c40618f0def0c98ec0a19
Tuukka Ojala <tuukka.ojala@iki.fi>
2024-01-10 11:09:46 +0200

    Päivitetty Kapsin polku

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-REMOTE_PATH=/home/users/tuukkao/sites/tuukkao.kapsi.fi/www
+REMOTE_PATH=/home/users/tuukkao/sites/tuukkao.net/www
 
 hugo --minify --gc
 rclone sync public/ kapsi:$REMOTE_PATH
